79c55bebdf4f79e7441cd47fb509a28c7a99ac4e
Sandoct <116573014+Sandoct@users.noreply.github.com>
2023-07-31 21:23:50 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gpouzet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 16:03:11 by gpouzet           #+#    #+#             */
-/*   Updated: 2023/07/14 19:18:37 by gpouzet          ###   ########.fr       */
+/*   Updated: 2023/07/31 12:17:17 by gpouzet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -15,11 +15,15 @@
 #include <readline/history.h>
 #include "minishell.h"
 
+int check_quote(char *readline); 
+
 void	show_data(t_data *data, int i)
 {
 	int	j;
+	t_list *tmp;
 
-	j = -1;
+	j = 0;
+	tmp = data->arg;
 	ft_printf("\n%d table\n", i);
 	if (data->input != NULL)
 		ft_printf("input : %s\n", data->input);
@@ -29,11 +33,25 @@ void	show_data(t_data *data, int i)
 		ft_printf("command : %s\n", data->command);
 	if (data->option != NULL)
 		ft_printf("option : %s\n", data->option);
-	if (data->arg != NULL)
-		while (data->arg[++j] != NULL)
-			ft_printf("arg%d : %s\n", j, data->arg[j]);
+	if (tmp != NULL)
+	{
+		while (tmp != NULL)
+		{
+			ft_printf("arg %d : %s\n", ++j, tmp->content);
+			tmp = tmp->next;
+		}
+	}
 	if (data->next != NULL)
 		show_data(data->next, ++i);
+}
+
+void	show_lexer(char **lexer)
+{
+	int	j;
+
+	j = -1;
+	while (lexer[++j])
+		ft_printf("arg %d : %s\n", j, lexer[j]);
 }
 
 int	main(void)
@@ -47,17 +65,22 @@ int	main(void)
 		str = readline(NULL);
 		if (str == NULL)
 			return (1);
-		lexer = ft_split(str, ' ');
+		if (ft_strncmp(str, "\0", 2))
+			add_history(str);
+		if (check_quote(str))
+			return(write(1, "error\n", 6));
+		lexer = arg_sep(str);
 		free(str);
 		if (lexer == NULL)
 			return (1);
+//		show_lexer(lexer);
 		data = parser(lexer);
-		freetab(lexer);
+//		freetab(lexer);
 		if (data == NULL)
 			return (1);
 		ft_printf("[	data table	]\n");
 		show_data(data, 1);
 		ft_printf("[	end		]\n");
-		clear_data(data);
+//		clear_data(data);
 	}
 }

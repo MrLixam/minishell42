56629ba4fc46d42d96570ed5b06d4257e9173d13
Liam VINCENT <lixam.vincent@gmail.com>
2023-09-25 21:48:58 +0200

    pipeline() finally under 25 lines

--- src/exec.c
+++ src/exec.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 16:36:55 by lvincent          #+#    #+#             */
-/*   Updated: 2023/09/25 16:11:06 by lvincent         ###   ########.fr       */
+/*   Updated: 2023/09/25 21:48:28 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 static char	**lst_to_str(t_list *lst, char *command)
 {
@@ -45,7 +46,7 @@ static void	clean_child(t_group *group, t_data *curr, int pipes[2], int fd)
 	close(pipes[1]);
 	close(fd);
 	clear_data(*group->line);
-	exit(EXIT_SUCCESS);
+	exit(EXIT_FAILURE);
 }
 
 static void	do_logic(int pipes[2], int fd, t_data *curr, t_group *group)
@@ -77,10 +78,30 @@ static void	do_logic(int pipes[2], int fd, t_data *curr, t_group *group)
 	clean_child(group, curr, pipes, fd);
 }
 
-void	ft_error(char *message)
+static void	ft_error(char *message, t_group *group)
 {
 	perror(message);
+	clear_data(*group->line);
+	free(group);
 	exit(EXIT_FAILURE);
+}
+
+static void	p_pass(t_group *group, t_data **curr, int *fd, int pipes[2])
+{
+	if (*curr != *group->line)
+		close(*fd);
+	if ((*curr)->next != NULL)
+	{
+		*fd = pipes[0];
+		close(pipes[1]);
+	}
+	*curr = (*curr)->next;
+}
+
+static void	close_pipe(int pipes[2])
+{
+	close(pipes[0]);
+	close(pipes[1]);
 }
 
 void	pipeline(t_group *group)
@@ -94,26 +115,16 @@ void	pipeline(t_group *group)
 	while (curr)
 	{
 		if (pipe(pipes) == -1)
-			ft_error("pipe failed");
+			ft_error("pipe failed", group);
 		fd_pid[1] = fork();
 		if (fd_pid[1] < 0)
-			ft_error("fork failed");
+			ft_error("fork failed", group);
 		else if (!fd_pid[1])
 			do_logic(pipes, fd_pid[0], curr, group);
 		else
-		{
-			if (curr != *group->line)
-				close(fd_pid[0]);
-			if (curr->next != NULL)
-			{
-				fd_pid[0] = pipes[0];
-				close(pipes[1]);
-			}
-			curr = curr->next;
-		}
+			p_pass(group, &curr, &fd_pid[0], pipes);
 	}
-	close(pipes[0]);
-	close(pipes[1]);
+	close_pipe(pipes);
 }
 /*
 int	main(int argc, char **argv, char **envp)
@@ -130,11 +141,15 @@ int	main(int argc, char **argv, char **envp)
 	line->next = new_data();
 	line->next->command = ft_strdup("/bin/grep");
 	line->next->arg = ft_lstnew(ft_strdup("main"));
-	line->next->next = NULL;
+	line->next->next = new_data();
+	line->next->next->command = ft_strdup("/bin/wc");
+	line->next->next->arg = ft_lstnew(ft_strdup("-l"));
+	line->next->next->next = NULL;
 	pipeline(group);
 	while (wait(NULL) > 0)
 		;
 	clear_data(line);
 	free(group);
 	return (0);
-}*/
+}
+*/

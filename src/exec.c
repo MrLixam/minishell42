75ec251d9a03a184e366e3a28729262d0c4f5e76
Liam VINCENT <lixam.vincent@gmail.com>
2023-09-25 15:39:16 +0200

    normed exec.c as much as possible, removed most leaks from the pipeline

--- src/exec.c
+++ src/exec.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lvincent <lvincent@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 16:36:55 by lvincent          #+#    #+#             */
-/*   Updated: 2023/09/21 01:10:25 by lvincent         ###   ########.fr       */
+/*   Updated: 2023/09/25 15:38:13 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-static char **lst_to_str(t_list *lst, char *command)
+static char	**lst_to_str(t_list *lst, char *command)
 {
-	char **str;
-	int i;
+	char	**str;
+	int		i;
 
 	i = 0;
 	str = ft_calloc(ft_lstsize(lst) + 2, sizeof(char *));
@@ -39,64 +39,97 @@ static int	file_access(char *path)
 	return (access(path, F_OK & R_OK & W_OK & X_OK));
 }
 
-void pipeline(t_data **line, char **envp)
+static void	clean_child(t_group *group, t_data *curr, int pipes[2], int fd)
 {
-	int pipes[2];
-	int fd;
-	int pid;
-	t_data *curr;
-	char **str;
+	clear_data(*group->line);
+	exit(EXIT_SUCCESS);
+}
 
-	curr = *line;
-	fd = STDIN_FILENO;
+static void	do_logic(int pipes[2], int fd, t_data *curr, t_group *group)
+{
+	char	**str;
+
+	if (curr != *group->line && curr->next != NULL)
+	{
+		dup2(fd, STDIN_FILENO);
+		dup2(pipes[1], STDOUT_FILENO);
+		close(fd);
+		close(pipes[0]);
+		close(pipes[1]);
+	}
+	else if (curr == *group->line)
+	{
+		dup2(pipes[1], STDOUT_FILENO);
+		close(pipes[0]);
+		close(pipes[1]);
+	}
+	else
+	{
+		dup2(fd, STDIN_FILENO);
+		close(fd);
+	}
+	str = lst_to_str(curr->arg, curr->command);
+	execve(curr->command, str, group->envp);
+	freetab(str);
+	clean_child(group, curr, pipes, fd);
+}
+
+void	ft_error(char *message)
+{
+	perror(message);
+	exit(EXIT_FAILURE);
+}
+
+void	pipeline(t_group *group)
+{
+	int		pipes[2];
+	int		fd_pid[2];
+	t_data	*curr;
+
+	curr = *group->line;
+	fd_pid[0] = STDIN_FILENO;
 	while (curr)
 	{
 		if (pipe(pipes) == -1)
-		{
-			perror("pipe failed");
-			exit(EXIT_FAILURE);
-		}
-		pid = fork();
-		if (pid < 0)
-		{
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
-		else if (!pid)
-		{
-			if (curr != *line && curr->next != NULL)
-			{
-				dup2(fd, STDIN_FILENO);
-				dup2(pipes[1], STDOUT_FILENO);
-				close(fd);
-				close(pipes[0]);
-				close(pipes[1]);
-			}
-			else if (curr == *line)
-			{
-				dup2(pipes[1], STDOUT_FILENO);
-				close(pipes[0]);
-				close(pipes[1]);
-			}
-			else
-			{
-				dup2(fd, STDIN_FILENO);
-				close(fd);
-			}
-			str = lst_to_str(curr->arg, curr->command);
-			execve(curr->command, str, envp);
-			freetab(str);
-		}
+			ft_error("pipe failed");
+		fd_pid[1] = fork();
+		if (fd_pid[1] < 0)
+			ft_error("fork failed");
+		else if (!fd_pid[1])
+			do_logic(pipes, fd_pid[0], curr, group);
 		else
 		{
-			if (curr != *line)
-				close(fd);
+			if (curr != *group->line)
+				close(fd_pid[0]);
 			if (curr->next != NULL)
 			{
-				fd = pipes[0];
+				fd_pid[0] = pipes[0];
 				close(pipes[1]);
 			}
 			curr = curr->next;
 		}
 	}
 }
+/*
+int	main(int argc, char **argv, char **envp)
+{
+	t_data		*line;
+	t_group		*group;
+
+	group = ft_calloc(1, sizeof(t_group));
+	group->line = &line;
+	group->envp = envp;
+	line = new_data();
+	line->command = ft_strdup("/bin/ls");
+	line->arg = ft_lstnew(ft_strdup("-l"));
+	line->next = new_data();
+	line->next->command = ft_strdup("/bin/grep");
+	line->next->arg = ft_lstnew(ft_strdup("main"));
+	line->next->next = NULL;
+	pipeline(group);
+	while (wait(NULL) > 0)
+		;
+	clear_data(line);
+	free(group);
+	return (0);
+}*/
--- src/minishell.h
+++ src/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lvincent <lvincent@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 13:59:09 by lvincent          #+#    #+#             */
-/*   Updated: 2023/09/15 16:27:24 by lvincent         ###   ########.fr       */
+/*   Updated: 2023/09/25 15:26:38 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,12 @@ typedef struct s_data
 	struct	s_data	*next;
 }	t_data;
 
+typedef struct s_group
+{
+	t_data **line;
+	char **envp;
+}	t_group;
+
 /*			prototype		*/
 void	freetab(char **tab);
 /*			data			*/
@@ -42,5 +48,5 @@ char	**arg_sep(char const *s);
 char	**env_sep(char const *s);
 t_data	*parser(char **lexer);
 /*			exec			*/
-void	pipeline(t_data **line, char **envp);
+void	pipeline(t_group *group);
 #endif
